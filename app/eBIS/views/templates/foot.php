fd77523fafe2ee360fefd37fc92e876e52d406e7
Isaac Yong <isaachawks@gmail.com>
2013-07-04 21:54:18 +0800

    Adjusted FB Twitter Button Positions
    o Centered and aligned the FB and Twitter buttons

--- app/eBIS/views/templates/foot.php
+++ app/eBIS/views/templates/foot.php
@@ -4,15 +4,30 @@
 			<br/>
 
 			<footer>
-				<a href="#" class="btn btn-fb btn-small"
-				onclick="
-			    window.open(
-			      'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href),
-			      'facebook-share-dialog',
-			      'width=626,height=436');
-			    return false;"><i class="icon-fb"></i> Share</a>
-
-			<a class="twitter-share-button" href="https://twitter.com/share" data-url="http://www.ebiscentre.com">Share on Twitter</a>
+				<div class="row">
+					<div class="span3"></div>
+					<div class="span5">
+						<h4>Share or Tweet this page!</h4>
+					</div>
+				</div>
+				<div class="row">
+					<div class="span3"></div>
+					<div class="span5">
+						<div class="fb span2">
+							<a href="#" class="btn btn-fb btn-small"
+							onclick="
+						    window.open(
+						      'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href),
+						      'facebook-share-dialog',
+						      'width=626,height=436');
+						    return false;"><i class="icon-fb"></i> Share</a>
+					    </div>
+				    	
+				    	<div class="twt span2">
+							<a class="twitter-share-button" href="https://twitter.com/share" data-url="http://www.ebiscentre.com">Share on Twitter</a>
+						</div>
+					</div>
+				</div>
 			</footer>
 
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
