621bb9f7f424f38df25109ac1e5955b83523a97d
Isaac Yong <isaachawks@gmail.com>
2013-07-19 16:55:10 +0800

    Animation Support Handling
    + Added JS functionality to redirect non Chrome/Safari browsers at Home
    to About Us page

--- app/eBIS/views/templates/head.php
+++ app/eBIS/views/templates/head.php
@@ -13,7 +13,20 @@
 <title><?php echo $title ?></title>
 
 <?php
-
+	if(current_url() == base_url() . 'index.php')
+	{
+?>
+		<script src="<?php echo base_url('js/browser-detect.js'); ?>"></script>
+		<script type="text/javascript">
+			if (BrowserDetect.browser != "Chrome" && BrowserDetect.browser != "Safari") {
+				alert("You are not using Chrome or Safari web browser! Redirecting..");
+				window.location = "<?php echo base_url() . 'index.php/about_us'; ?>"
+			};
+		</script>
+<?php
+	}
+?>
+<?php
 for ($i=0; $i < count($link); $i++) { 
      echo link_tag($link[$i]);
 }
